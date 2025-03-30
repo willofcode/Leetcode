8246c0722c82398658148aef35d8b2f3585876d9
William Ng <wng003@citymail.cuny.edu>
2025-03-30 00:37:48 -0400

    third

--- Arrays/consecutive.py
+++ Arrays/consecutive.py
@@ -14,7 +14,7 @@ class Solution:
             if num - 1 not in hash: # checks the prev num is not the set of numbers
                 curr = num      # intialize the current number as num
                 currsequence = 1   # intialize the first count for sequence
-                while curr + 1 in hash: # checks in next num in set (next consecutive number)
+                while curr + 1 in hash: # checks in next num in set (next consecutive number) (O(n) operation)
                     curr += 1 # incremenet current number by one
                     currsequence += 1 #  increment count for sequence
                 sequence = max(sequence, currsequence) # compares and find the longest sequence for consecutive
@@ -23,7 +23,7 @@ class Solution:
 
         # time complexity : O(n)
         # space complexity : O(n)
-        
+        # additionaly the  implementation with while loop may seem O(n*n) but in grand scheme of operations the actual operation of the algorithm scans and processed the numbers once hence the total time complexity is O(n+n) ~ 0(2n) ~ O(n)
         # implementation of nums.sort() can lower space complexity to O(1),
         # however tradeoff in time complexity, O(n log n)
         
