8f590f3f2e01867be8d867929c9a96d14db9a007
William Ng <165541107+willofcode@users.noreply.github.com>
2025-06-17 23:58:51 -0400

    Create groupanagram.py

---
+++ Hashmap/groupanagram.py
@@ -0,0 +1,13 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+      # initialize the hash table
+        res = defaultdict(list)
+        for s in strs:
+            count = [0] * 26 # initialize the array for 26 letters in the alphabet
+            for c in s: 
+                count[ord(c) - ord('a')] += 1 # increment count for each alphabet for each string in the array
+            res[tuple(count)].append(s) 
+        return list(res.values())
+
+  # time complexity: O(m * n)
+  # space complexity: O(m * n)
