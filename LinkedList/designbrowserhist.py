905894348347476af9bfbe713c430ae942ac0c83
William Ng <wng003@citymail.cuny.edu>
2025-11-22 18:01:54 -0500

    update browser history design

--- LinkedList/designbrowserhist.py
+++ LinkedList/designbrowserhist.py
@@ -1,5 +1,5 @@
 
-class BrowserHistory:
+#class BrowserHistory:
     # stack implementation
 #    def __init__(self, homepage: str):
 #        self.history = [homepage]
@@ -23,6 +23,36 @@ class BrowserHistory:
     
     # time complexity: O(1) for initialization and visit, O(n) for back and forward function in refrence to # of steps
     # space complexity: O(m * n) for n # of urls, and m average len of each url
+
+class Listnode:
+    def __init__(self, val, prev=None, next=None):
+        self.val = val
+        self.next = next
+        self.prev = prev
+
+class BrowserHistory:
+
+    def __init__(self, homepage: str):
+        self.curr = Listnode(homepage)
+
+    def visit(self, url: str) -> None:
+        self.curr.next = Listnode(url, self.curr)
+        self.curr = self.curr.next
+
+    def back(self, steps: int) -> str:
+        while self.curr.prev and steps > 0:
+            self.curr = self.curr.prev
+            steps -= 1
+        return self.curr.val
+
+    def forward(self, steps: int) -> str:
+        while self.curr.next and steps > 0:
+            self.curr = self.curr.next
+            steps -= 1
+        return self.curr.val
+
+# time complexity: O(1) init O(1) for visit(), O(min(n,steps)) forward and backward
+# space complexity: O(m * n) n for # of visited Urls and m # average length of url, and # of steps forward or backward
 
 
 # we can design using stack, Dynamic array, and Doubly LL
