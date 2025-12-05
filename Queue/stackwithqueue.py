c08d4f0a1457ccffe57d5b0f9025999304109cb7
William Ng <165541107+willofcode@users.noreply.github.com>
2025-12-04 23:59:37 -0500

    Add MyStack class with two queue implementation
    Implemented MyStack class using two queues for stack operations.

--- Queue/stackwithqueue.py
+++ Queue/stackwithqueue.py
@@ -1,19 +1,26 @@
 class MyStack:
-
+# two queue implementation
     def __init__(self):
-        
+        self.q1 = deque()
+        self.q2 = deque()
 
     def push(self, x: int) -> None:
-        
+        self.q2.append(x)
+        while self.q1:
+            self.q2.append(self.q1.popleft())
+
+        self.q1, self.q2 = self.q2, self.q1
 
     def pop(self) -> int:
-        
+        return self.q1.popleft()
 
     def top(self) -> int:
-        
+        return self.q1[0]
 
     def empty(self) -> bool:
-        
+        return len(self.q1) == 0
+
+# time s
 
 
 # Your MyStack object will be instantiated and called as such:
