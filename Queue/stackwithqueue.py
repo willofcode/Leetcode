5b488d53e1ae730edf333b312fd7881beb64a631
William Ng <165541107+willofcode@users.noreply.github.com>
2025-12-05 23:59:48 -0500

    Update stackwithqueue.py
    Alternative method

--- Queue/stackwithqueue.py
+++ Queue/stackwithqueue.py
@@ -22,8 +22,31 @@ class MyStack:
 
 # time complexity: O(n) push, O(1) pop/ initialized
 # space complexity: O(n)
+'''
+# alternative implementation using one queue
+# note deque stands for double ended queue, the library
+# utilizes linked list for optimal implementation of queue
 
+class MyStack:
 
+    def __init__(self):
+        self.q = deque()
+
+    def push(self, x: int) -> None:
+        self.q.append(x)
+        for _ in range(len(self.q) - 1):
+            self.q.append(self.q.popleft())
+
+    def pop(self) -> int:
+        return self.q.popleft()
+
+    def top(self) -> int:
+        return self.q[0]
+
+    def empty(self) -> bool:
+        return len(self.q) == 0
+        
+'''
 # Your MyStack object will be instantiated and called as such:
 # obj = MyStack()
 # obj.push(x)
