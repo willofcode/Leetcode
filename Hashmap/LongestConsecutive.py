77c62416c35f765815fc760b63189ef3fd639418
William Ng <165541107+willofcode@users.noreply.github.com>
2025-12-11 23:54:20 -0500

    Enhance comments in longestConsecutive method
    Added comments to explain sorting implementation and alternate approach.

--- Hashmap/LongestConsecutive.py
+++ Hashmap/LongestConsecutive.py
@@ -1,5 +1,6 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
+        # sorting implementation
         if not nums:
             return 0
         res = 0
@@ -17,3 +18,20 @@ class Solution:
             curr += 1
             res = max(res, streak)
         return res
+        # Time complexity: O(n log n) due to sort()
+        # Space complexity: O(n) or O(1) dependend on sorting algorithm
+
+        # alternate implementation
+        # numSet = set(nums)
+        # longest = 0
+
+        # for num in numSet:
+        #     if (num - 1) not in numSet:
+        #         length = 1
+        #         while (num + length) in numSet:
+        #             length += 1
+        #         longest = max(length, longest)
+        # return longest
+
+        # time complexity: O(n)
+        # space complexity: O(n)
