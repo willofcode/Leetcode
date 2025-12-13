892aac66583d6ef00338afb7cdb76db9c8ae5211
William Ng <165541107+willofcode@users.noreply.github.com>
2025-12-12 23:54:57 -0500

    Comment out alternate hashmap implementation
    Commented out an alternate implementation using a hashmap for finding the longest consecutive sequence.

--- Hashmap/LongestConsecutive.py
+++ Hashmap/LongestConsecutive.py
@@ -35,3 +35,18 @@ class Solution:
 
         # time complexity: O(n)
         # space complexity: O(n)
+
+        # alternate implementation with hashmap
+        # mp = defaultdict(int)
+        # res = 0
+
+        # for num in nums:
+        #     if not mp[num]:
+        #         mp[num] = mp[num - 1] + mp[num + 1] + 1
+        #         mp[num - mp[num - 1]] = mp[num]
+        #         mp[num + mp[num + 1]] = mp[num]
+        #         res = max(res, mp[num])
+        # return res
+
+        # time complexity: O(n)
+        # space complexity: O(n)
